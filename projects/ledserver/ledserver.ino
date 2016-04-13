8101db54357a6a65dceb9e2fa53ebe8ac8a633e1
Matth96 <Matthias.Dolstra@hva.nl>
2016-04-13 14:44:53 +0200

    update file so post and get works :)

--- projects/ledserver/ledserver.ino
+++ projects/ledserver/ledserver.ino
@@ -1,19 +1,28 @@
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
 
-const char* ssid     = "hio";  
-const char* password = "hio!";
+const char* ssid     = "Lisa";  
+const char* password = "sayLISA1993";
 
 const char* host     = "iot.dolstra.me"; // Your domain  
-String path          = "/api/status/matthias";  
-const int ledPin        = D0;
+String path          = "/api/status/lisa";  
+const int httpPort   = 80;
+const int ledPin     = D0;
+const int ButtonPin  = D1;
+int buttonValue      = 0;
+
+WiFiClient client;
 
 void setup() {  
-  
+  //set pins 
+  pinMode(ButtonPin, INPUT); 
   pinMode(ledPin, OUTPUT); 
   pinMode(ledPin, HIGH);
+
+  //set serial
   Serial.begin(9600);
 
+  //set wifi
   delay(10);
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -24,22 +33,29 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
-
+  //show when wifi is connected
   Serial.println("WiFi connected");  
   Serial.println("IP address: " + WiFi.localIP());
 }
 
 void loop() {  
-  Serial.print("connecting to ");
-  Serial.println(host);
-  WiFiClient client;
-  const int httpPort = 80;
+   buttonValue = digitalRead(ButtonPin);
+  
   if (!client.connect(host, httpPort)) {
     Serial.println("connection failed");
     return;
   }
+   
+   getNetworkData();
 
-  client.print(String("GET ") + path + " HTTP/1.1\r\n" +
+      delay(2000);
+   sendNetworkData();
+ 
+}
+
+
+void getNetworkData(){
+   client.print(String("GET ") + path + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" + 
                "Connection: keep-alive\r\n\r\n");
 
@@ -85,5 +101,32 @@ void loop() {
     }
   }
   Serial.print("closing connection. ");
+}
+
+
+void sendNetworkData() {
+  
+  if (client.connect(host,httpPort)) { 
+  String postStr = "lisa=";
+  
+  if (buttonValue == 1){
+        postStr += "true";
+  }else{
+     postStr += "false";
+  }
+  
+  Serial.print(buttonValue);
+    client.println("POST /api HTTP/1.1");
+    client.println("Host: " + String(host));
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.println("Connection: close");
+    client.print("Content-Length: ");
+    client.println(postStr.length());
+    client.println();
+    client.print(postStr);
+    client.println();
+    Serial.println("Data send");
+
+  } 
 }
 
