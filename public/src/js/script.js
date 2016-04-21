96140526014cdfa90ba2245e3f41822935d96ef9
Senny Kalidien <senny@directzichtbaar.nl>
2016-04-21 23:33:39 +0200

    Update script

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -1,18 +1,26 @@
 d3.json("/api/data", function(error, data) {
     var time = [];
-    var sensor = ['sensor'];
+    var sensor = ['Distance from laptop to me'];
 
     data.forEach(function(element, index) {
         time.push(element.time);
         sensor.push(element.input.distance);
-        console.log(sensor);
     });
+
+    var showResults = 30;
+    var newArr = sensor.slice(Math.max(sensor.length - showResults, 1));
+    var text = 'Distance from laptop in centimeter';
+
+    newArr.unshift(text);
+
+    console.log(newArr);
 
     var chart = c3.generate({
         data: {
             columns: [
-                sensor
-            ]
+                newArr
+            ],
+            xFormat: '%H:%M:%S'
         },
         axis: {
             x: {
