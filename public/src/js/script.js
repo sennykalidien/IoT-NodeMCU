70877eb698d0b77ef4b8dfb55f0164b662818ea1
Senny Kalidien <senny@directzichtbaar.nl>
2016-05-15 18:15:18 +0200

    Little changes

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -12,17 +12,26 @@ function outputUpdateGrandpa(vol) {
 
 d3.json("/api/data", function(error, data) {
     var time = ['times'];
-    var sensor = ['Distance from laptop to me'];
+    var sensor = ['Distance from object to sensor'];
+    var settingsRed = ['Settings for red LED'];
+    var settingsGreen = ['Settings for green LED'];
 
     data.forEach(function(element, index) {
         time.push(element.time);
         sensor.push(element.input.distance);
+        settingsRed.push(element.settings.red);
+        settingsGreen.push(element.settings.green);
     });
 
     var showResults = 30;
     var newArr = sensor.slice(Math.max(sensor.length - showResults, 1));
-    var text = 'Distance from laptop in centimeter';
+    var text = 'Distance from object in sensor';
     var lastDistance  = sensor.slice(-1);
+    var lastSettingsRed  = settingsRed.slice(-1);
+    var lastSettingsGreen  = settingsGreen.slice(-1);
+
+    console.log(lastSettingsRed);
+
 
     newArr.unshift(text);
 
@@ -71,11 +80,11 @@ d3.json("/api/data", function(error, data) {
             width: 50 // for adjusting arc thickness
     },
     color: {
-        pattern: ['e74c3c', '#e74c3c', '#f1c40f', '#2ecc71'], // the three color levels for the percentage values.
+        pattern: ['#e74c3c', '#e74c3c', '#f1c40f', '#2ecc71'], // the three color levels for the percentage values.
         threshold: {
             unit: 'value',
             max: 250, // 100 is default
-            values: [0, 50, 70, 100]
+            values: [0, lastSettingsRed, lastSettingsGreen]
         }
     },
     bindto: '#pie'
